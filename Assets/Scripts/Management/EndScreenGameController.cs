a72950b9997855e3257c19a04d55461c0cfafec6
Valérian Perez <v.perez@office525.o365.ovh.com>
2019-08-13 14:45:34 +0200

    [VPE] Add the three scenes (title, main, end) and GameController/Manager

---
+++ Assets/Scripts/Management/EndScreenGameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreenGameController : MainMenuController
+{
+
+    #region Unity Built In
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    #endregion Unity Built In
+}
---
+++ Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the current game controller
+    /// </summary>
+    public GameController GameController;
+
+    #endregion Properties
+
+    #region Unity Built In
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    #endregion Unity Built In
+}
