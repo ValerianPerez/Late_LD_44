9c14310515db4b811f2f6d65974a133ba7fedc27
SandraBenard <52406865+SandraBenard@users.noreply.github.com>
2019-08-13 11:15:23 +0200

    PlayerControllerV0.1

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public bool useController;
+    Vector3 inputDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //follow the mouse
+        if (!useController)
+        {
+            var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        //with controller
+        if(useController)
+        {
+            inputDirection = Vector3.zero;
+            inputDirection.x = Input.GetAxisRaw("LHorizontal");
+            inputDirection.y = Input.GetAxisRaw("LVertical");
+            if(inputDirection != Vector3.zero)
+            { 
+            var angle = Mathf.Atan2(inputDirection.x, inputDirection.y) * Mathf.Rad2Deg - 180f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+           
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerScript/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movespeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public bool useController;
+    Animator playerAnimator;
+    private int countItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            movement = Vector2.zero;
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            UpdateAnimationAndMove();
+    }
+
+    void MoveCharacter()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * movespeed * Time.deltaTime);
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (movement != Vector2.zero)
+        {
+            MoveCharacter();
+            playerAnimator.SetFloat("Horizontal", movement.x);
+            playerAnimator.SetFloat("Vertical", movement.y);
+            playerAnimator.SetBool("moving", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("moving", false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("interactableObject"))
+        {
+            other.gameObject.SetActive(false);
+            countItem = countItem + 1;
+        }
+    }
+}
