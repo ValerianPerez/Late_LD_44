ba1318c1a43715298a6cd981304456d6e9c063b0
SandraBenard <52406865+SandraBenard@users.noreply.github.com>
2019-08-14 16:10:41 +0200

    EnnemySBD

---
+++ Assets/Scripts/Ennemy/Screamer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screamer : MonoBehaviour
+{
+    public GameObject ennemy;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ennemy.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ennemy.SetActive(false);
+    }
+}
